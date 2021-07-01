b06f1c4c6f7105cd4313261b41c47cd2abd3d64d
Ellen <710394505@qq.com>
2021-07-01 15:59:12 +0800

    add: 归并排序 自顶向上 递归处理

--- src/com/vy/sort/MergeSort.java
+++ src/com/vy/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package com.vy.sort;
 
+import java.util.Arrays;
+
 /**
  * @author: Ellen
  * @Date: 2021/7/1 14:48
@@ -7,9 +9,72 @@ package com.vy.sort;
  */
 public class MergeSort {
 
-	public void merge(){
+	public static void main(String[] args) {
 
+		int[] a = {1, 5, 2, 6, 3, 7, 4, 8, 5, 9, 0};
 
+		sort(a, 0, a.length - 1);
+
+	}
+
+	public static void sort(int[] a, int lo, int hi) {
+
+		if (lo >= hi) {
+			return;
+		}
+		int mid = lo + (hi - lo) / 2;
+
+		sort(a, lo, mid);
+
+		sort(a, mid + 1, hi);
+
+		merge(a, lo, mid, hi);
+
+		System.out.println(Arrays.toString(a));
+	}
+
+	public static void merge(int[] a, int left, int mid, int right) {
+
+		int ri = mid + 1;
+
+		int le = left;
+
+		int[] as = new int[a.length];
+
+		for (int i = left; i <= right; i++) {
+
+			as[i] = a[i];
+
+		}
+
+		for (int i = left; i <= right; i++) {
+			if (le > mid) {
+
+				a[i] = as[ri];
+
+				ri++;
+			} else if (ri > right) {
+
+				a[i] = as[le];
+
+				le++;
+
+			} else if (as[le] < as[ri]) {
+
+				a[i] = as[le];
+
+				le++;
+			} else {
+
+				a[i] = as[ri];
+
+				ri++;
+
+			}
+
+		}
+
+		return ;
 
 	}
 
